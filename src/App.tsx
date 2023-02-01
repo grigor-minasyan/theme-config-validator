b06ee54152316e0a1c1a871d054f7e47fb611a8c
Grigor Minasyan <grigorminasyan7@gmail.com>
2023-02-01 14:04:47 -0800

    added json editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,179 +1,89 @@
-import { useEffect, useState } from "react";
-import { z } from "zod";
-import zodToJsonSchema from "zod-to-json-schema";
-
-const Sizing = z.object({
-  desktop: z.object({ width: z.number(), height: z.number() }),
-  mobile: z.object({ width: z.number(), height: z.number() }),
-});
-
-const Position = z.object({
-  desktop: z.object({ right: z.number(), bottom: z.number() }),
-  mobile: z.object({ right: z.number(), bottom: z.number() }),
-});
-
-const ThemeConfig = z.object({
-  isDefault: z.boolean().optional(),
-  fonts: z.object({
-    primaryFontUrl: z.string(),
-    boldFontUrl: z.string(),
-    defaultFontUrl: z.string(),
-  }),
-  images: z.object({
-    loadingImage: z.string(),
-    heroBanner: z.string(),
-    unexpectedErrorIcon: z.string(),
-    sessionTimeoutIcon: z.string(),
-    macroArticleIcon: z.string(),
-  }),
-  colors: z.object({
-    primary: z.string(),
-    secondary: z.string(),
-    background: z.object({
-      main: z.string(),
-      light: z.string(),
-      accent: z.string(),
-      header: z.string(),
-      disabled: z.string(),
-      element: z.string(),
-    }),
-    hoverBackground: z.object({
-      primaryHover: z.string(),
-      secondaryHover: z.string(),
-      accentHover: z.string(),
-    }),
-    messageBackgrounds: z.object({
-      outbound: z.string(),
-      inbound: z.string(),
-    }),
-    text: z.object({
-      primaryLight: z.string(),
-      secondaryLight: z.string(),
-      primaryDark: z.string(),
-      secondaryDark: z.string(),
-      disabled: z.string(),
-      buttonRegular: z.string(),
-      buttonHover: z.string(),
-      error: z.string(),
-    }),
-    border: z.object({
-      default: z.string(),
-      dividerOnDark: z.string(),
-      active: z.string(),
-      error: z.string(),
-      disabled: z.string(),
-    }),
-    controls: z.object({
-      uncheckedControl: z.string(),
-      inboundChecked: z.string(),
-      outboundChecked: z.string(),
-      disabledControl: z.string(),
-    }),
-    launcher: z
-      .object({
-        background: z.string().optional(),
-        hoverBackground: z.string().optional(),
-      })
-      .optional(),
-    link: z.object({
-      inboundDefaultLink: z.string(),
-      inboundDisabledLink: z.string(),
-      inboundHoverLink: z.string(),
-      outboundDefaultLink: z.string(),
-      outboundDisabledLink: z.string(),
-      outboundHoverLink: z.string(),
-    }),
-    ratingIcon: z.object({
-      INBOUND: z.string(),
-      OUTBOUND: z.string(),
-    }),
-    boxShadow: z.string(),
-  }),
-  text: z.object({
-    partnerFriendlyName: z.string(),
-    uppercaseButtonText: z.boolean(),
-  }),
-  launcher: z.object({
-    initializeFromLauncher: z.boolean(),
-    imageSrc: z.string(),
-    position: Position.optional(),
-    sizing: Sizing,
-  }),
-  mainWidget: z
-    .object({
-      position: Position,
-      sizing: Sizing.optional(),
-    })
-    .optional(),
-});
+import { useRef, useState } from "react";
+import Editor, { type OnChange } from "@monaco-editor/react";
+import type { editor } from "monaco-editor";
+import { initialThemeConfigStr, ThemeConfig, zErrorFormatter } from "./utils";
 
 function App() {
-  const [widgetIds, setWidgetIds] = useState([
-    "rbi_bk_internal",
-    "keen_footwear",
-    "keen_chat_trial",
-    "solostove_us",
-    "hum_nutrition",
-  ]);
-  const [themeConfigs, setThemeConfigs] = useState<Record<string, object>>({});
-  const jsonSchema = JSON.stringify(
-    zodToJsonSchema(ThemeConfig, "ThemeConfigSchema").definitions
-      .ThemeConfigSchema,
-    null,
-    8
-  );
-  useEffect(() => {
-    (async () => {
-      for (const widgetId of widgetIds) {
-        const res = await fetch(
-          `https://app.gosimplr.com/chat-api/v1/chat/v2/themeConfig?widgetId=${widgetId}&schemaVersion=2`
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const [errors, setErrors] = useState("Edit to see if there are any errors");
+  const [find, setFind] = useState("");
+  const [replace, setReplace] = useState("");
+  const handleFindChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFind(e.target.value);
+  };
+  const handleReplaceChange: React.ChangeEventHandler<HTMLInputElement> = (
+    e
+  ) => {
+    setReplace(e.target.value);
+  };
+
+  const handleFindAndReplace = () => {
+    if (editorRef.current) {
+      editorRef.current
+        .getModel()
+        ?.setValue(
+          editorRef.current?.getModel()?.getValue().replaceAll(find, replace) ||
+            ""
         );
-        const data = await res.json();
-        setThemeConfigs((prevState) => ({
-          ...prevState,
-          [widgetId]: data?.themeConfig,
-        }));
+    }
+  };
+
+  const handleFormat = () => {
+    if (editorRef.current) {
+      const value = editorRef.current?.getModel()?.getValue();
+      if (!value) {
+        return;
       }
-    })();
-  }, []);
+      let formattedValue = "";
+      try {
+        formattedValue = JSON.stringify(JSON.parse(value), null, 2);
+      } catch (e) {
+        return;
+      }
+      editorRef.current.getModel()?.setValue(formattedValue);
+    }
+  };
+
+  const handleEditorChange: OnChange = (value, event) => {
+    if (!value) {
+      setErrors("no value was provided");
+      return;
+    }
+    let themeConfigParsed;
+    try {
+      themeConfigParsed = JSON.parse(value);
+    } catch (e) {
+      setErrors("no valid JSON was provided");
+      return;
+    }
+    const zResults = ThemeConfig.safeParse(themeConfigParsed);
+    if (!zResults.success) {
+      setErrors(zErrorFormatter(zResults.error.format()));
+    }
+  };
+
   return (
-    <div>
-      {widgetIds.map((widgetId) => {
-        const result = ThemeConfig.safeParse(themeConfigs[widgetId]);
-        let errorText = "";
-        if (!result.success) {
-          // handle error then return
-          errorText = "";
-          result.error.issues.forEach((issue) => {
-            errorText +=
-              "Code: " +
-              issue.code +
-              "\t\t" +
-              " Expected: " +
-              //@ts-ignore
-              issue.expected +
-              "\t\t" +
-              " Path: " +
-              issue.path.join(".") +
-              "\n";
-          });
-        } else {
-          // do something
-          errorText = "no error";
-        }
-
-        return (
-          <div key={widgetId}>
-            <h2>{widgetId}</h2>
-            <p style={{ whiteSpace: "pre" }}>
-              {themeConfigs[widgetId] ? errorText : "Loading..."}
-            </p>
-          </div>
-        );
-      })}
-
-      <h1>Here is the schema used</h1>
-      <p style={{ whiteSpace: "pre" }}>{jsonSchema}</p>
+    <div style={{ display: "flex" }}>
+      <Editor
+        height="95vh"
+        width="50vw"
+        defaultLanguage="json"
+        defaultValue={initialThemeConfigStr}
+        onChange={handleEditorChange}
+        onMount={(editor, monaco) => (editorRef.current = editor)}
+      />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex" }}>
+          <input placeholder="Quick find" onChange={handleFindChange}></input>
+          <input
+            placeholder="And replace"
+            onChange={handleReplaceChange}
+          ></input>
+          <button onClick={handleFindAndReplace}>Find and replace</button>
+          <button onClick={handleFormat}>Format if valid json</button>
+        </div>
+        <div style={{ whiteSpace: "pre" }}>{errors}</div>
+      </div>
     </div>
   );
 }
---
+++ src/utils.ts
@@ -0,0 +1,252 @@
+import { z, ZodFormattedError } from "zod";
+
+const Sizing = z.object({
+  desktop: z.object({ width: z.number(), height: z.number() }),
+  mobile: z.object({ width: z.number(), height: z.number() }),
+});
+
+const Position = z.object({
+  desktop: z.object({ right: z.number(), bottom: z.number() }).strict(),
+  mobile: z.object({ right: z.number(), bottom: z.number() }).strict(),
+});
+
+const Color = z
+  .string()
+  .regex(/(?:#|0x)(?:[a-f0-9]{3}|[a-f0-9]{6})\b|(?:rgb|hsl)a?\([^\)]*\)/i);
+
+export const ThemeConfig = z.object({
+  isDefault: z.boolean().optional(),
+  fonts: z
+    .object({
+      primaryFontUrl: z.string(),
+      boldFontUrl: z.string(),
+      defaultFontUrl: z.string(),
+    })
+    .strict(),
+  images: z
+    .object({
+      loadingImage: z.string(),
+      heroBanner: z.string(),
+      unexpectedErrorIcon: z.string(),
+      sessionTimeoutIcon: z.string(),
+      macroArticleIcon: z.string(),
+      tooltip: z.string().optional(),
+    })
+    .strict(),
+  colors: z
+    .object({
+      primary: Color,
+      secondary: Color,
+      background: z
+        .object({
+          main: Color,
+          light: Color,
+          accent: Color,
+          header: Color,
+          disabled: Color,
+          element: Color,
+        })
+        .strict(),
+      hoverBackground: z
+        .object({
+          primaryHover: Color,
+          secondaryHover: Color,
+          accentHover: Color,
+        })
+        .strict(),
+      messageBackgrounds: z
+        .object({
+          outbound: Color,
+          inbound: Color,
+        })
+        .strict(),
+      text: z
+        .object({
+          primaryLight: Color,
+          secondaryLight: Color,
+          primaryDark: Color,
+          secondaryDark: Color,
+          disabled: Color,
+          buttonRegular: Color,
+          buttonHover: Color,
+          error: Color,
+        })
+        .strict(),
+      border: z
+        .object({
+          default: Color,
+          dividerOnDark: Color,
+          active: Color,
+          error: Color,
+          disabled: Color,
+        })
+        .strict(),
+      controls: z
+        .object({
+          uncheckedControl: Color,
+          inboundChecked: Color,
+          outboundChecked: Color,
+          disabledControl: Color,
+        })
+        .strict(),
+      launcher: z
+        .object({
+          background: Color.optional(),
+          hoverBackground: Color.optional(),
+        })
+        .strict()
+        .optional(),
+      link: z
+        .object({
+          inboundDefaultLink: Color,
+          inboundDisabledLink: Color,
+          inboundHoverLink: Color,
+          outboundDefaultLink: Color,
+          outboundDisabledLink: Color,
+          outboundHoverLink: Color,
+        })
+        .strict(),
+      ratingIcon: z
+        .object({
+          INBOUND: Color,
+          OUTBOUND: Color,
+        })
+        .strict(),
+      boxShadow: Color,
+    })
+    .strict(),
+  text: z
+    .object({
+      partnerFriendlyName: z.string(),
+      uppercaseButtonText: z.boolean(),
+      routeToAgentMessage: z.string().optional(),
+    })
+    .strict(),
+  launcher: z
+    .object({
+      initializeFromLauncher: z.boolean(),
+      imageSrc: z.string(),
+      position: Position.optional(),
+      sizing: Sizing,
+    })
+    .strict(),
+  mainWidget: z
+    .object({
+      position: Position,
+      sizing: Sizing.optional(),
+    })
+    .strict()
+    .optional(),
+});
+
+export const zErrorFormatter = (
+  error: ZodFormattedError<z.infer<typeof ThemeConfig>>,
+  depth = 0,
+  finalStrs = [],
+  curKey = "Global"
+) => {
+  Object.entries(error).forEach(([key, val]) => {
+    if (key === "_errors") {
+      // @ts-expect-error
+      finalStrs.push("----".repeat(depth) + " " + curKey + " | " + val[0]);
+    } else {
+      // @ts-expect-error
+      zErrorFormatter(error[key], depth + 1, finalStrs, key);
+    }
+  });
+  return finalStrs.join("\n");
+};
+
+export const initialThemeConfigStr = `colors: {
+  primary: '#000000',
+  secondary: '#60269E',
+
+
+  background: {
+    main: '#FFFFFF',
+    light: '#FFFFFFF',
+    accent: '#60269E’,
+    disabled: '#EDEDED',
+    element: '#EDEDED',
+    header: '#000000’,
+  },
+
+
+  hoverBackground: {
+    primaryHover: '#F1E9F9’,
+    secondaryHover: '#292929',
+    accentHover: '#7947AD',
+  },
+
+
+  messageBackgrounds: {
+    outbound: '#F4F4F4',
+    inbound: '#60269E',
+  },
+
+
+  text: {
+    primaryDark: '#000000',
+    secondaryDark: '#757575',
+    primaryLight: '#FFFFFF',
+    secondaryLight: '#E4CBFF',
+    disabled: '#ACACAC',
+    buttonRegular: '#60269E',
+    buttonHover: '#7947AD',
+    error: '#FF6D6D',
+    qr: '#60269E',
+  },
+
+
+  border: {
+    default: '#D4D4D4’,
+    active: '#60269E’,
+    disabled: '#D4D4D4’,
+    error: '#FF6D6D’,
+    dividerOnDark: '#7947AD’,
+  },
+
+
+  controls: {
+    uncheckedControl: ‘#ACACAC’,
+    inboundChecked: '#FFFFFF’,
+    outboundChecked: '#60269E’,
+    disabledControl: '#D4D4D4’,
+  },
+  
+  
+  ratingIcon: {
+    INBOUND: '#F9D51C’,
+    OUTBOUND: '#000000’,
+  },
+
+
+  launcher: {
+    background: ‘#60269E’,
+    hoverBackground: ‘#7947AD’,
+    imageSrc: ,
+    initializeFromLauncher: true,
+    boxShadow: '0px 12px 16px 0px ##40166D (24%)',
+  },
+
+
+ link: {       inboundDefaultLink: '#FFFFFF',       outboundDefaultLink: '#3115F2’,
+    inboundHoverLink: '#FFFFFF',       outboundHoverLink: '#21137A’,
+    inboundDisabledLink: '#FFFFFF (56%)’,       outboundDisabledLink: '#5C5C5C’,
+  },
+
+
+  accent: {
+    preTransitAccent: '#EF6C00', 
+    inTransitAccent: '#512DA8’,
+    successAccent: '#388E3C’,
+    noInfoAccent: '#757575’,
+  },
+
+
+  system: {
+    errorIcon: '#EC5427', 
+    errorHoverIcon: '#DA3F11’,
+  },
+
+}`;
